4a242474838f207353ba0a9543dd4b3f3717068c
Chris Burrell <chrisburrell@gmail.com>
2018-11-21 14:19:53 -0600

    fixup to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-    bucket = "terraform-state-bucket-choosy-moms-ui"
-    key = "states/choosy-moms-ui.tfstate"
-    dynamodb_table = "terraform-lock"
-    region = "us-west-2"
-  }
-}
-
 provider "template" {
   version = "~> 1.0"
 }
@@ -15,6 +5,13 @@ provider "template" {
 provider "aws" {
   region = "${var.aws_region}"
   version = "~> 1.24"
-  max_retries = 5
-  allowed_account_ids = ["575020237982"]
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "terraform-state-bucket-choosy-moms"
+    key = "states/choosy-moms-ui.tfstate"
+    region = "us-east-1"
+  }
 }
