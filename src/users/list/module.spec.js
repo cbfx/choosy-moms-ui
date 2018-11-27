b1c0414fbd7f57df3208b5c70264f8a3f1df0b71
Chris Burrell <chrisburrell@gmail.com>
2018-11-27 06:59:21 -0600

    feat(users): remove module

--- src/users/list/module.spec.js
+++
@@ -1,8 +0,0 @@
-import module from './module';
-import config from './config';
-
-describe(`${module.name} module`, function() {
-  it('should load successfully', function() {
-    expect(module.name).toBe(config.NAMESPACE);
-  });
-});
