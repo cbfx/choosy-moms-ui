cce943aa8a3f7f4d1734b0cef26b0aa7b3934c5e
Chris Burrell <chrisburrell@gmail.com>
2018-11-27 10:23:47 -0600

    feat(collections): add module

---
+++ src/collections/api/data-service.js
@@ -0,0 +1,33 @@
+import config from './config';
+
+export const API_BASE_PATH = `${config.API.basePath}`;
+export const API_FULL_PATH = `${API_BASE_PATH}/${config.API.version}`;
+
+export const API_LIST_KEY = 'collections';
+export const API_LIST_INDEX_KEY = 'userId';
+export const API_RESOURCE_LIST_PATH = `${API_FULL_PATH}/${API_LIST_KEY}`;
+export const API_RESOURCE_DETAIL_PATH = `${API_RESOURCE_LIST_PATH}/:${API_LIST_INDEX_KEY}`;
+
+/*
+* Check out
+* {@link ?? API Repo}
+* for more information.
+*/
+
+export default function($resource) {
+  return $resource(
+    API_RESOURCE_LIST_PATH,
+    {
+      API_LIST_INDEX_KEY: `@${API_LIST_INDEX_KEY}`
+    },
+    {
+      get: {
+        method: 'GET'
+      },
+      query: {
+        method: 'GET',
+        isArray: false
+      }
+    }
+  );
+};
