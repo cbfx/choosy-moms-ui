e3ae8b72320440aa6024ebf788415122fe55c66f
Chris Burrell <chrisburrell@gmail.com>
2018-11-27 07:00:37 -0600

    feat(index): change homepage layout

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.scss';
 
 import config from './config';
 
-import usersListModule from './users/list/module';
+import authModule from './auth/module';
+import searchListModule from './search/list/module';
 
 const appDependencies = [
   'ngRoute'
@@ -16,18 +17,12 @@ if (__USE_MOCKS__) {
   console.info('%c____USING FAKE SSO____', 'background: #222; color: #1ff1f5');
 }
 
-appDependencies.push(usersListModule.name);
+appDependencies.push(authModule.name);
+appDependencies.push(searchListModule.name);
 
 angular.module(`${config.NAMESPACE}`, appDependencies)
   .controller('appCtrl', function() {
     this.appName = config.TITLE;
-    this.routes = [
-      {
-        href: '/users',
-        label: 'Users'
-      }
-    ];
-
     return this;
   })
   .constant('DATE_FORMAT_STRING', DATE_FORMAT_STRING)
